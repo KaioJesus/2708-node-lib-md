5475125bcc6f96ab65a1ae1ab6ab6cde869c3d5a
KaioJramos <kaioramos990@hotmail.com>
2024-02-28 10:16:18 -0300

    pacote chalk

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('olá mundo');
+// const chalk = require('chalk') // forma antiga de importar
+import chalk from 'chalk'; //importando o arquivo
+
+console.log(chalk.blue('olá mundo'));
+console.log(chalk.blue('Hello world!'));
